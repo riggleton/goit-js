5d3e1aa8d030642d82b22408e82f68f905d62c25
riggleton <39995318+riggleton@users.noreply.github.com>
2020-04-28 22:04:21 +0300

    hw-08

--- goit-js-hw-08-gallery/gallery.js
+++ goit-js-hw-08-gallery/gallery.js
@@ -6,13 +6,38 @@ const galleryMarkUp = galleryItems.reduce(renderImage, "");
 const modal = document.querySelector("div.js-lightbox");
 const lightbox = document.querySelector("img.lightbox__image");
 const closeBtn = document.querySelector('button[data-action="close-lightbox"]');
-// const overlay = document.querySelector("div.lightbox__overlay");
-// console.log(overlay);
+const overlay = document.querySelector('div.lightbox__content');
 
 gallery.insertAdjacentHTML("afterbegin", galleryMarkUp);
 gallery.addEventListener("click", handleGalleryClick);
 closeBtn.addEventListener("click", handleModalCloseClick);
-// overlay.addEventListener("click", handleModalCloseClick);
+overlay.addEventListener("click", handleModalCloseClick);
+window.addEventListener('keydown', onKeyPress);
+
+function onKeyPress(event) {
+  let index;
+  switch(event.keyCode) {
+    case 27:
+      modal.classList.remove("is-open");
+      lightbox.setAttribute("src", "");
+      break;
+    case 39:
+      index = galleryItems.findIndex(item => item.original === lightbox.src);
+      if (index === galleryItems.length - 1) {
+        index = 0;
+      }
+      openOriginal(galleryItems[index+1].original);
+      break;
+    case 37:
+      index = galleryItems.findIndex(item => item.original === lightbox.src);
+      if (index <= 0) {
+        index = galleryItems.length - 1;
+      }
+      openOriginal(galleryItems[index-1].original);
+      break;
+  }
+}
+
 
 function handleGalleryClick(event) {
   event.preventDefault();
@@ -24,10 +49,12 @@ function handleGalleryClick(event) {
 
 function openOriginal(link) {
   modal.classList.add("is-open");
-  lightbox.setAttribute("src", link);
+  // lightbox.setAttribute("src", link);
+  lightbox.src = link;
 }
 
 function handleModalCloseClick(event) {
+  if (event.target === lightbox) return;
   modal.classList.remove("is-open");
   lightbox.setAttribute("src", "");
 }
